d3dd04ba4971617c0aef07b04c4c5c1ee19429fb
linghaosong <linghao815@gmail.com>
2025-09-10 09:59:04 -0700

    lab2

--- lab2/src/knn.cpp
+++ lab2/src/knn.cpp
@@ -2,65 +2,71 @@
 #include <tapa.h>
 #include "knn.h"
 
-using std::cout;
-using std::endl;
+//using std::cout;
+//using std::endl;
 
 const int kbytes_img = 3072;
 // each image has 3072 bytes (32*32*3)
 
 void read_image(
-  tapa::mmap<uint8_t> img_mem,
+  tapa::mmap<uint32_t> img_mem,
   const int img_num,
-  tapa::ostream<uint8_t> &q_out,
+  tapa::ostream<uint32_t> &q_out,
   const int rp_time
 ) {
   for (int rp = 0; rp < rp_time; rp++) {
-    for (int i = 0; i < img_num * kbytes_img; i++) {
+    for (int i = 0; i < img_num * kbytes_img / 4; i++) {
+    #pragma HLS PIPELINE II=1
       q_out.write(img_mem[i]);
     }
   }
 }
 
 void knn(
-  tapa::istream<uint8_t> &q_in_0,
-  tapa::istream<uint8_t> &q_in_1,
-  tapa::istream<uint8_t> &q_in_2,
-  tapa::istream<uint8_t> &q_in_3,
-  tapa::istream<uint8_t> &q_in_4,
-  tapa::istream<uint8_t> &q_in_5,
-  tapa::istream<uint8_t> &q_in_6,
-  tapa::istream<uint8_t> &q_in_7,
-  tapa::istream<uint8_t> &q_in_8,
-  tapa::istream<uint8_t> &q_in_9,
-  tapa::istream<uint8_t> &q_test,
-  tapa::ostream<uint8_t> &q_prediction,
+  tapa::istream<uint32_t> &q_in_0,
+  tapa::istream<uint32_t> &q_in_1,
+  tapa::istream<uint32_t> &q_in_2,
+  tapa::istream<uint32_t> &q_in_3,
+  tapa::istream<uint32_t> &q_in_4,
+  tapa::istream<uint32_t> &q_in_5,
+  tapa::istream<uint32_t> &q_in_6,
+  tapa::istream<uint32_t> &q_in_7,
+  tapa::istream<uint32_t> &q_in_8,
+  tapa::istream<uint32_t> &q_in_9,
+  tapa::istream<uint32_t> &q_test,
+  tapa::ostream<uint32_t> &q_prediction,
   const int test_image_num,
   const int train_image_each_class_num
 ){
   for (int t = 0; t < test_image_num; t++) {
-    cout << "t = " << t << endl;
-    uint8_t best_label = 0;
+    //cout << "t = " << t << endl;
+    int best_label = 0;
     int best_dist = 0x7FFFFFFF;
-    uint8_t test_img[kbytes_img];
-    for (int i = 0; i < kbytes_img; i++) {
+    uint32_t test_img[kbytes_img / 4];
+    for (int i = 0; i < kbytes_img / 4; i++) {
+    #pragma HLS PIPELINE II=1
       test_img[i] = q_test.read();
     }
     for (int tr = 0; tr < train_image_each_class_num; tr++) {
       for (int c = 0; c < 10; ++c) {
         int dist = 0;
-        for (int i = 0; i < kbytes_img; ++i) {
-          int16_t d = (int16_t)(c == 0 ? q_in_0.read() :
-                                c == 1 ? q_in_1.read() :
-                                c == 2 ? q_in_2.read() :
-                                c == 3 ? q_in_3.read() :
-                                c == 4 ? q_in_4.read() :
-                                c == 5 ? q_in_5.read() :
-                                c == 6 ? q_in_6.read() :
-                                c == 7 ? q_in_7.read() :
-                                c == 8 ? q_in_8.read() :
-                                         q_in_9.read())
-                  - (int16_t)test_img[i];
-          dist += int(d * d);
+        for (int i = 0; i < kbytes_img / 4; ++i) {
+        #pragma HLS PIPELINE II=1
+          uint32_t train_image_4byte = (c == 0 ? q_in_0.read() :
+                                        c == 1 ? q_in_1.read() :
+                                        c == 2 ? q_in_2.read() :
+                                        c == 3 ? q_in_3.read() :
+                                        c == 4 ? q_in_4.read() :
+                                        c == 5 ? q_in_5.read() :
+                                        c == 6 ? q_in_6.read() :
+                                        c == 7 ? q_in_7.read() :
+                                        c == 8 ? q_in_8.read() :
+                                                 q_in_9.read());
+          for (int p = 0; p < 4; p++) {
+            int d = (int)((train_image_4byte >> (p * 8)) & 0xFF)
+                  - (int)((test_img[i] >> (p * 8)) & 0xFF);
+            dist += d * d;
+          }
         }
         if (dist < best_dist) {
           best_dist = dist;
@@ -73,53 +79,79 @@ void knn(
 }
 
 void write_label(
-  tapa::istream<uint8_t> &q_in,
-  tapa::mmap<uint8_t> label_mem,
-  const int img_num
+  tapa::istream<uint32_t> &q_in,
+  tapa::mmap<uint32_t> label_mem,
+  const int img_num,
+  tapa::ostream<bool> &q_done
 ) {
   for (int i = 0; i < img_num; i++) {
+  #pragma HLS PIPELINE II=1
     label_mem[i] = q_in.read();
   }
+  q_done.write(true);
+}
+
+void timer(tapa::istream<bool> &q,
+           tapa::mmap<uint32_t> mem) {
+  uint32_t cycle_count = 0;
+  while (q.empty()) {
+  #pragma HLS PIPELINE II=1
+    ++cycle_count;
+  }
+  q.read();
+  mem[0] = cycle_count;
 }
 
 void KNNKernel(
-  tapa::mmap<uint8_t> train_image_0,
-  tapa::mmap<uint8_t> train_image_1,
-  tapa::mmap<uint8_t> train_image_2,
-  tapa::mmap<uint8_t> train_image_3,
-  tapa::mmap<uint8_t> train_image_4,
-  tapa::mmap<uint8_t> train_image_5,
-  tapa::mmap<uint8_t> train_image_6,
-  tapa::mmap<uint8_t> train_image_7,
-  tapa::mmap<uint8_t> train_image_8,
-  tapa::mmap<uint8_t> train_image_9,
-  tapa::mmap<uint8_t> test_image,
-  tapa::mmap<uint8_t> predict_label,
+  tapa::mmap<uint32_t> train_image_0,
+  tapa::mmap<uint32_t> train_image_1,
+  tapa::mmap<uint32_t> train_image_2,
+  tapa::mmap<uint32_t> train_image_3,
+  tapa::mmap<uint32_t> train_image_4,
+  tapa::mmap<uint32_t> train_image_5,
+  tapa::mmap<uint32_t> train_image_6,
+  tapa::mmap<uint32_t> train_image_7,
+  tapa::mmap<uint32_t> train_image_8,
+  tapa::mmap<uint32_t> train_image_9,
+  tapa::mmap<uint32_t> test_image,
+  tapa::mmap<uint32_t> predict_label,
+  tapa::mmap<uint32_t> cycle_count,
   const int test_image_num,
   const int train_image_each_class_num
 ) {
-  tapa::streams<uint8_t, 10, 2> q_tr_img("q_trin_image");
-  // for tapa::streams, the parameters to use is <data_type, num_streams, depth>
+  tapa::stream<uint32_t, 2> q_tr_img_0("q_trin_image_0");
+  tapa::stream<uint32_t, 2> q_tr_img_1("q_trin_image_1");
+  tapa::stream<uint32_t, 2> q_tr_img_2("q_trin_image_2");
+  tapa::stream<uint32_t, 2> q_tr_img_3("q_trin_image_3");
+  tapa::stream<uint32_t, 2> q_tr_img_4("q_trin_image_4");
+  tapa::stream<uint32_t, 2> q_tr_img_5("q_trin_image_5");
+  tapa::stream<uint32_t, 2> q_tr_img_6("q_trin_image_6");
+  tapa::stream<uint32_t, 2> q_tr_img_7("q_trin_image_7");
+  tapa::stream<uint32_t, 2> q_tr_img_8("q_trin_image_8");
+  tapa::stream<uint32_t, 2> q_tr_img_9("q_trin_image_9");
 
-  tapa::stream<uint8_t, 2> q_t_img("q_test_image");
-  tapa::stream<uint8_t, 2> q_label("predict_label");
+  tapa::stream<uint32_t, 2> q_t_img("q_test_image");
+  tapa::stream<uint32_t, 2> q_label("predict_label");
+
+  tapa::stream<bool, 2> q_done("q_done");
 
   tapa::task()
-    .invoke(read_image, train_image_0, train_image_each_class_num, q_tr_img[0], test_image_num)
-    .invoke(read_image, train_image_1, train_image_each_class_num, q_tr_img[1], test_image_num)
-    .invoke(read_image, train_image_2, train_image_each_class_num, q_tr_img[2], test_image_num)
-    .invoke(read_image, train_image_3, train_image_each_class_num, q_tr_img[3], test_image_num)
-    .invoke(read_image, train_image_4, train_image_each_class_num, q_tr_img[4], test_image_num)
-    .invoke(read_image, train_image_5, train_image_each_class_num, q_tr_img[5], test_image_num)
-    .invoke(read_image, train_image_6, train_image_each_class_num, q_tr_img[6], test_image_num)
-    .invoke(read_image, train_image_7, train_image_each_class_num, q_tr_img[7], test_image_num)
-    .invoke(read_image, train_image_8, train_image_each_class_num, q_tr_img[8], test_image_num)
-    .invoke(read_image, train_image_9, train_image_each_class_num, q_tr_img[9], test_image_num)
+    .invoke(read_image, train_image_0, train_image_each_class_num, q_tr_img_0, test_image_num)
+    .invoke(read_image, train_image_1, train_image_each_class_num, q_tr_img_1, test_image_num)
+    .invoke(read_image, train_image_2, train_image_each_class_num, q_tr_img_2, test_image_num)
+    .invoke(read_image, train_image_3, train_image_each_class_num, q_tr_img_3, test_image_num)
+    .invoke(read_image, train_image_4, train_image_each_class_num, q_tr_img_4, test_image_num)
+    .invoke(read_image, train_image_5, train_image_each_class_num, q_tr_img_5, test_image_num)
+    .invoke(read_image, train_image_6, train_image_each_class_num, q_tr_img_6, test_image_num)
+    .invoke(read_image, train_image_7, train_image_each_class_num, q_tr_img_7, test_image_num)
+    .invoke(read_image, train_image_8, train_image_each_class_num, q_tr_img_8, test_image_num)
+    .invoke(read_image, train_image_9, train_image_each_class_num, q_tr_img_9, test_image_num)
     .invoke(read_image, test_image, test_image_num, q_t_img, 1)
-    .invoke(knn, q_tr_img[0], q_tr_img[1], q_tr_img[2], q_tr_img[3], q_tr_img[4],
-                 q_tr_img[5], q_tr_img[6], q_tr_img[7], q_tr_img[8], q_tr_img[9],
+    .invoke(knn, q_tr_img_0, q_tr_img_1, q_tr_img_2, q_tr_img_3, q_tr_img_4,
+                 q_tr_img_5, q_tr_img_6, q_tr_img_7, q_tr_img_8, q_tr_img_9,
                  q_t_img, q_label,
                  test_image_num, train_image_each_class_num)
-    .invoke(write_label, q_label, predict_label, test_image_num)
+    .invoke(write_label, q_label, predict_label, test_image_num, q_done)
+    .invoke(timer, q_done, cycle_count)
     ;
 }
--- lab2/src/main.cpp
+++ lab2/src/main.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include <iostream>
 #include <string>
+#include <cmath>
 
 #include "knn.h"
 
@@ -18,10 +19,11 @@ DEFINE_string(btstm, "", "path to the bitstream file, run csim if empty");
 DEFINE_string(data, "./cifar-10", "path to the CIFA10 binary data folder");
 DEFINE_int32(train_num, 32, "number of training images per class");
 DEFINE_int32(test_num, 32, "number of test images");
-DEFINE_bool(cput, true, "tune host CPU KNN performance if true");
+DEFINE_bool(skipk, true, "skip kernel execution, only host CPU if true");
 
 //read binary file and store the content into one vector uint8_t
-void ReadBinaryFile(const string &filename, aligned_vector<uint8_t> &data) {
+template <typename T>
+void ReadBinaryFile(const string &filename, aligned_vector<T> &data) {
   std::ifstream file(filename, std::ios::binary);
   if (!file) {
     throw std::runtime_error("Cannot open file: " + filename);
@@ -42,12 +44,12 @@ void ReadBinaryFile(const string &filename, aligned_vector<uint8_t> &data) {
 // we use 1-NN, K=1
 void KNN_host(std::vector<aligned_vector<uint8_t> > & train_image,
               aligned_vector<uint8_t> & test_image,
-              aligned_vector<uint8_t> & predict_label,
+              aligned_vector<uint32_t> & predict_label,
               const int test_image_num,
               const int train_image_each_class_num = 100) {
   predict_label.resize(test_image_num);
   for (int t = 0; t < test_image_num; t++) {
-    uint8_t best_label = 0;
+    int best_label = 0;
     int best_dist = 0x7FFFFFFF;
     for (int tr = 0; tr < train_image_each_class_num; tr++) {
       for (int c = 0; c < 10; ++c) {
@@ -69,9 +71,9 @@ void KNN_host(std::vector<aligned_vector<uint8_t> > & train_image,
   }
 }
 
-void Verify_predcition_accuracy(
-    aligned_vector<uint8_t> & test_label,
-    aligned_vector<uint8_t> & predict_label) {
+float Verify_predcition_accuracy(
+    aligned_vector<uint32_t> & test_label,
+    aligned_vector<uint32_t> & predict_label) {
   const int test_image_num = predict_label.size();
   int correct = 0;
   for (int i = 0; i < test_image_num; i++) {
@@ -81,6 +83,7 @@ void Verify_predcition_accuracy(
   clog << "Accuracy: "
        << (float)correct / (float)test_image_num * 100.0f
        << "%" << endl;
+  return (float)correct / (float)test_image_num;
 }
 
 bool end_with(const std::string &value, const std::string &ending) {
@@ -99,11 +102,11 @@ int main(int argc, char** argv) {
 
   //read cifar-10 test images and labels
   aligned_vector<uint8_t> test_image;
-  aligned_vector<uint8_t> test_label;
+  aligned_vector<uint32_t> test_label;
   ReadBinaryFile(FLAGS_data + "/test_image.bin", test_image);
   ReadBinaryFile(FLAGS_data + "/test_label.bin", test_label);
 
-  aligned_vector<uint8_t> predict_label;
+  aligned_vector<uint32_t> predict_label;
 
   // add a timer to measure host KNN performance
   steady_clock::time_point t1 = steady_clock::now();
@@ -114,9 +117,9 @@ int main(int argc, char** argv) {
 
   // veryfy host KNN prediction aginst test label
   clog << "Verifying host KNN (on CPU) prediction accuracy..." << endl;
-  Verify_predcition_accuracy(test_label, predict_label);
+  float acc_cpu = Verify_predcition_accuracy(test_label, predict_label);
 
-  if (FLAGS_cput) {
+  if (FLAGS_skipk) {
     return EXIT_SUCCESS;
   }
 
@@ -134,29 +137,41 @@ int main(int argc, char** argv) {
     return EXIT_FAILURE;
   }
 
+  aligned_vector<uint32_t> cycle_count(1);
+
   time_taken =
   tapa::invoke(KNNKernel, 
                FLAGS_btstm,
-               tapa::read_only_mmap<uint8_t>(train_image[0]),
-               tapa::read_only_mmap<uint8_t>(train_image[1]),
-               tapa::read_only_mmap<uint8_t>(train_image[2]),
-               tapa::read_only_mmap<uint8_t>(train_image[3]),
-               tapa::read_only_mmap<uint8_t>(train_image[4]),
-               tapa::read_only_mmap<uint8_t>(train_image[5]),
-               tapa::read_only_mmap<uint8_t>(train_image[6]),
-               tapa::read_only_mmap<uint8_t>(train_image[7]),
-               tapa::read_only_mmap<uint8_t>(train_image[8]),
-               tapa::read_only_mmap<uint8_t>(train_image[9]),
-               tapa::read_only_mmap<uint8_t>(test_image),
-               tapa::write_only_mmap<uint8_t>(predict_label),
+               tapa::read_only_mmap<uint8_t>(train_image[0]).reinterpret<uint32_t>(),
+               tapa::read_only_mmap<uint8_t>(train_image[1]).reinterpret<uint32_t>(),
+               tapa::read_only_mmap<uint8_t>(train_image[2]).reinterpret<uint32_t>(),
+               tapa::read_only_mmap<uint8_t>(train_image[3]).reinterpret<uint32_t>(),
+               tapa::read_only_mmap<uint8_t>(train_image[4]).reinterpret<uint32_t>(),
+               tapa::read_only_mmap<uint8_t>(train_image[5]).reinterpret<uint32_t>(),
+               tapa::read_only_mmap<uint8_t>(train_image[6]).reinterpret<uint32_t>(),
+               tapa::read_only_mmap<uint8_t>(train_image[7]).reinterpret<uint32_t>(),
+               tapa::read_only_mmap<uint8_t>(train_image[8]).reinterpret<uint32_t>(),
+               tapa::read_only_mmap<uint8_t>(train_image[9]).reinterpret<uint32_t>(),
+               tapa::read_only_mmap<uint8_t>(test_image).reinterpret<uint32_t>(),
+               tapa::write_only_mmap<uint32_t>(predict_label),
+               tapa::write_only_mmap<uint32_t>(cycle_count),
                FLAGS_test_num,
                FLAGS_train_num);
   
   clog << "KNN kernel execution time: " << time_taken * 1e-6 << " millisecond" << endl;
+  clog << "KNN kernel cycle count: " << cycle_count[0] << endl;
 
   // veryfy KNN kernel prediction aginst test label
   clog << "Verifying KNN kernel prediction accuracy..." << endl;
-  Verify_predcition_accuracy(test_label, predict_label);
+  float acc_kernel = Verify_predcition_accuracy(test_label, predict_label);
+
+  if (std::abs(acc_cpu - acc_kernel) * 100.0f > 0.1f) {
+    clog << "Accuracy mismatch: KNN kernel accuracy " << acc_kernel * 100.0f
+         << "% differs from host CPU accuracy " << acc_cpu * 100.0f
+         << "% by more than 0.1%" << endl;
+  } else {
+    clog << "KNN kernel test PASS!" << endl;
+  }
   
   return EXIT_SUCCESS;
 }

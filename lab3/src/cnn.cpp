43164bd12eeee6d6fbd81b69cae7727486e3c08a
linghaosong <linghao815@gmail.com>
2025-09-29 17:04:51 -0700

    tripcount

--- lab3/src/cnn.cpp
+++ lab3/src/cnn.cpp
@@ -2,13 +2,6 @@
 #include <tapa.h>
 #include "cnn.h"
 
-//to help hls estimation
-constexpr int MaxkNum       = kNum_0;       // chnannel number
-constexpr int MaxkKernel    = kKernel_0;    // knernel size
-constexpr int MaxkImSize    = kImSize_0;    //image size (after conv)
-constexpr int MaxkInImSize  = kInImSize_0;  //input image size
-constexpr int MaxkOutImSize = kOutImSize_0; //output image size (after maxpool)
-
 /*
 void read_input(tapa::mmap<float>...,
                 tapa::ostream<float>...,
@@ -38,7 +31,7 @@ void cnn_core(tapa::istream<float>...,
 ...
 */
 
-void CnnKernel(
+void cnncore(
   tapa::mmap<float> in_img,
   tapa::mmap<float> weight,
   tapa::mmap<float> bias,
@@ -51,11 +44,11 @@ void CnnKernel(
   static float C[kNum_0][kImSize_0][kImSize_0];
 
   for (int i = 0; i < kNum; ++i) {
-  #pragma HLS loop_tripcount min=1 max=MaxkNum
+  #pragma HLS loop_tripcount min=1 max=kNum_0
     for (int h = 0; h < kImSize; ++h) {
-    #pragma HLS loop_tripcount min=1 max=MaxkImSize
+    #pragma HLS loop_tripcount min=1 max=kImSize_0
       for (int w = 0; w < kImSize; ++w) {
-      #pragma HLS loop_tripcount min=1 max=MaxkImSize
+      #pragma HLS loop_tripcount min=1 max=kImSize_0
         C[i][h][w] = bias[i];
       }
     }
@@ -63,17 +56,17 @@ void CnnKernel(
 
   // Convolution
   for (int i = 0; i < kNum; ++i) {
-  #pragma HLS loop_tripcount min=1 max=MaxkNum
+  #pragma HLS loop_tripcount min=1 max=kNum_0
     for (int j = 0; j < kNum; ++j) {
-    #pragma HLS loop_tripcount min=1 max=MaxkNum
+    #pragma HLS loop_tripcount min=1 max=kNum_0
       for (int h = 0; h < kImSize; ++h) {
-      #pragma HLS loop_tripcount min=1 max=MaxkImSize
+      #pragma HLS loop_tripcount min=1 max=kImSize_0
         for (int w = 0; w < kImSize; ++w) {
-        #pragma HLS loop_tripcount min=1 max=MaxkImSize
+        #pragma HLS loop_tripcount min=1 max=kImSize_0
           for (int p = 0; p < kKernel; ++p) {
-          #pragma HLS loop_tripcount min=1 max=MaxkKernel
+          #pragma HLS loop_tripcount min=1 max=kKernel_0
             for (int q = 0; q < kKernel; ++q) {
-            #pragma HLS loop_tripcount min=1 max=MaxkKernel
+            #pragma HLS loop_tripcount min=1 max=kKernel_0
               C[i][h][w] += weight(i, j, p, q) * in_img(j, h + p, w + q);
             }
           }
@@ -84,11 +77,11 @@ void CnnKernel(
 	
 	// ReLU
 	for (int i = 0; i < kNum; ++i) {
-  #pragma HLS loop_tripcount min=1 max=MaxkNum
+  #pragma HLS loop_tripcount min=1 max=kNum_0
     for (int h = 0; h < kImSize; ++h) {
-    #pragma HLS loop_tripcount min=1 max=MaxkImSize
+    #pragma HLS loop_tripcount min=1 max=kImSize_0
       for (int w = 0; w < kImSize; ++w) {
-      #pragma HLS loop_tripcount min=1 max=MaxkImSize
+      #pragma HLS loop_tripcount min=1 max=kImSize_0
         C[i][h][w] = max(0.f, C[i][h][w]);
       }
     }
@@ -96,16 +89,30 @@ void CnnKernel(
 	
 	// Max pooling
   for (int i = 0; i < kNum; ++i) {
-  #pragma HLS loop_tripcount min=1 max=MaxkNum
+  #pragma HLS loop_tripcount min=1 max=kNum_0
     for (int h = 0; h < kOutImSize; ++h) {
-    #pragma HLS loop_tripcount min=1 max=MaxkOutImSize
+    #pragma HLS loop_tripcount min=1 max=kOutImSize_0
       for (int w = 0; w < kOutImSize; ++w) {
-      #pragma HLS loop_tripcount min=1 max=MaxkOutImSize
+      #pragma HLS loop_tripcount min=1 max=kOutImSize_0
         out_img(i, h, w) = max(
           max(C[i][h * 2][w * 2    ], C[i][h * 2 + 1][w * 2    ]),
           max(C[i][h * 2][w * 2 + 1], C[i][h * 2 + 1][w * 2 + 1]));
       }
     }
   }
+}
+
+void CnnKernel(
+  tapa::mmap<float> in_img,
+  tapa::mmap<float> weight,
+  tapa::mmap<float> bias,
+  tapa::mmap<float> out_img,
+  const int kNum,
+  const int kKernel,
+  const int kImSize,
+  const int kInImSize,
+  const int kOutImSize) {
+  tapa::task()
+    .invoke(cnncore, in_img, weight,  bias, out_img, kNum, kKernel, kImSize, kInImSize, kOutImSize);
 }
 
